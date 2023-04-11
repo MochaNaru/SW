da085b0ca3866a2f0551e8dd728280c84e33e128
Mocha_NARU <113840611+MochaNaru@users.noreply.github.com>
2023-04-11 13:21:23 +0900

    Add files via upload

---
+++ 0411/0411cal.py
@@ -0,0 +1,16 @@
+'''
+2023-04-11
+정성욱
+'''
+num1=int(input("첫번째 숫자 입력:"))
+op=input("연산자 입력:")
+num2=int(input("두번째 숫자 입력:"))
+
+if op == '+': #조건1
+    print(num1+num2) #조건1 이가 참일 경우  
+elif ("op == -"): #조건2
+    print(num1-num2) #조건2 이가 참일 경우
+elif ('op == *'): #조건3
+    print(num1*num2) #조건 3이가 참일 경우
+else:
+    print(num1/num2) #조건 1,2,3 이가 다 거짓인 경우
